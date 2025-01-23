32eee3f1287cb5c9e3c1e9a9c545e1f663dea68f
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2025-01-23 17:48:06 +0900

    feat: 질문 초기 데이터 생성

--- backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/entity/Question.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/entity/Question.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/service/QuestionService.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.NBE3_4_2_Team4.domain.board.question.service;
 
+import com.NBE3_4_2_Team4.domain.board.question.entity.Question;
 import com.NBE3_4_2_Team4.domain.board.question.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class QuestionService {
     private final QuestionRepository questionRepository;
+
+    public long count() {
+        return questionRepository.count();
+    }
+
+    public Question write(String title, String content) {
+        return questionRepository.save(Question.builder()
+                .title(title)
+                .content(content)
+                .build());
+    }
 }
