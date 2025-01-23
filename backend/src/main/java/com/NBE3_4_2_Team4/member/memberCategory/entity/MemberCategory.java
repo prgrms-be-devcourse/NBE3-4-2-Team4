26ac85c1a5158f0a99757c4a7f0a459793b83a5b
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-01-23 22:36:18 +0900

    feat- memberCategory 엔티티 추가 (관리자/ 유저 등 구분을 위함)

---
+++ backend/src/main/java/com/NBE3_4_2_Team4/member/memberCategory/entity/MemberCategory.java
@@ -0,0 +1,13 @@
+package com.NBE3_4_2_Team4.member.memberCategory.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class MemberCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
