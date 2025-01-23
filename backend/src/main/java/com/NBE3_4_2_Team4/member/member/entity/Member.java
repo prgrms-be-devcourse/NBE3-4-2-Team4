519e9435deb6c3797349750146f27bf8a29a316e
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-01-23 22:37:23 +0900

    feat- member의 필드에 memberCategory 연관 관계 생성

--- backend/src/main/java/com/NBE3_4_2_Team4/member/member/entity/Member.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/member/member/entity/Member.java
@@ -1,5 +1,6 @@
 package com.NBE3_4_2_Team4.member.member.entity;
 
+import com.NBE3_4_2_Team4.member.memberCategory.entity.MemberCategory;
 import jakarta.persistence.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,6 +13,10 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JoinColumn
+    @ManyToOne
+    private MemberCategory memberCategory;
 
     @Column(nullable = false, unique = true)
     private String email;
