5b997a5c5de639140bfcb481f03160e46fb0bc2f
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-01-24 16:04:18 +0900

    feat- 토큰 값으로부터 Claims를 얻어오는 기능 추가 - 예외 처리 미완성

--- backend/src/main/java/com/NBE3_4_2_Team4/global/jwt/JwtManager.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/global/jwt/JwtManager.java
@@ -1,14 +1,16 @@
 package com.NBE3_4_2_Team4.global.jwt;
 
 import com.NBE3_4_2_Team4.member.member.entity.Member;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Base64;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtManager {
@@ -18,7 +20,7 @@ public class JwtManager {
     @Value("${jwt.secret.valid.minute : 30}")
     private static long jwtValidMinute;
 
-    private final Key key;
+    private final SecretKey key;
 
     public JwtManager(){
         byte[] keyBytes = Base64.getDecoder().decode(jwtSecretKey);
@@ -34,5 +36,25 @@ public class JwtManager {
                 .expiration(new Date(System.currentTimeMillis() + jwtValidMinute * 60 * 1000))
                 .signWith(key)
                 .compact();
+    }
+
+    public Map<String, Object> getClaims(String token) {
+        if (token == null || token.isEmpty()) {
+            throw new JwtException("Token is empty");
+        }
+        try {
+            Claims claims = Jwts.parser()
+                    .decryptWith(key)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+            return new HashMap<>(claims);
+        }catch (ExpiredJwtException e){
+            throw new JwtException("Token is expired");
+        }catch (UnsupportedJwtException e){
+            throw new JwtException("Token is empty");
+        }catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e){
+            throw new JwtException("Token is empty2");
+        }
     }
 }
