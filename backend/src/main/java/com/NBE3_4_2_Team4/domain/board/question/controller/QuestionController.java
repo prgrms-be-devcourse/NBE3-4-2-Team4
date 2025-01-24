bcb7dab3282fadcfa321b7d5f24a6d8608a5b8a0
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2025-01-24 16:08:08 +0900

    SCRUM-63 feat: 글 등록 구현

--- backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/controller/QuestionController.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/controller/QuestionController.java
@@ -3,13 +3,16 @@ package com.NBE3_4_2_Team4.domain.board.question.controller;
 import com.NBE3_4_2_Team4.domain.board.question.entity.Question;
 import com.NBE3_4_2_Team4.domain.board.question.service.QuestionService;
 import com.NBE3_4_2_Team4.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/questions")
+@RequestMapping("/api/questions")
 @RequiredArgsConstructor
 public class QuestionController {
     private final QuestionService questionService;
@@ -30,6 +33,22 @@ public class QuestionController {
         return new RsData<>(
                 "200-1",
                 "게시글 삭제가 완료되었습니다."
+        );
+    }
+
+    record QuestionCreateReqBody(
+            @NotNull @Length(min = 2)
+            String title,
+            @NotNull @Length(min = 2)
+            String content
+    ) {}
+
+    @PostMapping
+    public RsData<Void> write(@RequestBody @Valid QuestionCreateReqBody reqBody) {
+        Question q = questionService.write(reqBody.title, reqBody.content);
+        return new RsData<>(
+                "200-1",
+                "%d번 게시글 생성이 완료되었습니다.".formatted(q.getId())
         );
     }
 }
