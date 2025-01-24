e4a15299f406efac82e0d02b6f231e477642e0ab
solji <solji.choi89@gmail.com>
2025-01-24 14:29:39 +0900

    SCRUM-40 feat- 답변 엔티티 클래스 생성

---
+++ backend/src/main/java/com/NBE3_4_2_Team4/global/jpa/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.NBE3_4_2_Team4.global.jpa.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
+    @Setter(AccessLevel.PROTECTED)
+    @EqualsAndHashCode.Include
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    private Long id;
+
+}
