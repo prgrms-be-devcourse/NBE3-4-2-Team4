098b3ce495326438fabea1cc9fb3e1c05a12a814
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-01-24 10:07:40 +0900

    refactor- 안쓰는 코드 및 import 삭제

--- backend/src/main/java/com/NBE3_4_2_Team4/global/config/SecurityConfig.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/global/config/SecurityConfig.java
@@ -3,22 +3,14 @@ package com.NBE3_4_2_Team4.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
-
-    private List<RequestMatcher> needAuthenticated(String pattern){
-
-    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
