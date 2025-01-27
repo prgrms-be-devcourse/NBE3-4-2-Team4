290f9f4e45e7a8aceab225f3a6ef691ca7665d81
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-01-27 10:46:31 +0900

    feat- 비밀번호 불일치 시 400 응답을 반환하는 기능 추가 (로그인 관련이라 전역 예외 처리 말고 컨트롤러 내부에 뒀습니다.)

--- backend/src/main/java/com/NBE3_4_2_Team4/domain/member/member/controller/MemberController.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/member/member/controller/MemberController.java
@@ -2,10 +2,15 @@ package com.NBE3_4_2_Team4.domain.member.member.controller;
 
 import com.NBE3_4_2_Team4.domain.member.dto.request.LoginRequestDto;
 import com.NBE3_4_2_Team4.domain.member.member.service.MemberService;
+import com.NBE3_4_2_Team4.global.exceptions.InValidPasswordException;
 import com.NBE3_4_2_Team4.global.rsData.RsData;
 import com.NBE3_4_2_Team4.global.security.HttpManager;
+import com.NBE3_4_2_Team4.standard.base.Empty;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
     private final MemberService memberService;
     private final HttpManager httpManager;
+
+    @ExceptionHandler(InValidPasswordException.class)
+    public ResponseEntity<RsData<Empty>> handleInValidPasswordException(InValidPasswordException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new RsData<>(
+                        "400-2",
+                        e.getMessage()
+                ));
+    }
 
     @PostMapping("/api/login")
     public RsData<String> login(
--- backend/src/main/java/com/NBE3_4_2_Team4/global/exceptions/InValidPasswordException.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/global/exceptions/InValidPasswordException.java
@@ -2,6 +2,6 @@ package com.NBE3_4_2_Team4.global.exceptions;
 
 public class InValidPasswordException extends RuntimeException {
     public InValidPasswordException() {
-        super("Invalid password");
+        super("비밀번호가 일치하지 않습니다.");
     }
 }
