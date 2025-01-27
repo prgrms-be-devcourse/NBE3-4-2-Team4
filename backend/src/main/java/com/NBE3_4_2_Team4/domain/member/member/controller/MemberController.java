98422656f032ec61015e35e1dbe67b6e6180b8cd
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-01-27 10:15:48 +0900

    feat- 사이트 자체의 로그인 관련 컨트롤러 매서드 추가

--- backend/src/main/java/com/NBE3_4_2_Team4/domain/member/member/controller/MemberController.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/member/member/controller/MemberController.java
@@ -1,15 +1,27 @@
 package com.NBE3_4_2_Team4.domain.member.member.controller;
 
+import com.NBE3_4_2_Team4.domain.member.dto.request.LoginRequestDto;
 import com.NBE3_4_2_Team4.domain.member.member.service.MemberService;
+import com.NBE3_4_2_Team4.global.rsData.RsData;
+import com.NBE3_4_2_Team4.global.security.HttpManager;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final HttpManager httpManager;
 
-    @PostMapping
-
+    @PostMapping("/api/login")
+    public RsData<String> login(
+            @RequestBody LoginRequestDto loginRequestDto,
+            HttpServletResponse resp) {
+        String token = memberService.login(loginRequestDto);
+        httpManager.setCookie(resp, "auth", token, 30);
+        return new RsData<>("200-1", "OK", token);
+    }
 }
