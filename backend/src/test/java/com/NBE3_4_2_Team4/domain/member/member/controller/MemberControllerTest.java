49356898d8a5580877ccef0528b8920551cd705b
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-01-27 10:28:59 +0900

    test- 로그인 성공 테스트 추가

---
+++ backend/src/test/java/com/NBE3_4_2_Team4/domain/member/member/controller/MemberControllerTest.java
@@ -0,0 +1,43 @@
+package com.NBE3_4_2_Team4.domain.member.member.controller;
+
+import com.NBE3_4_2_Team4.domain.member.dto.request.LoginRequestDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Transactional
+public class MemberControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void loginSuccessTest() throws Exception {
+        LoginRequestDto loginRequestDto = LoginRequestDto.builder()
+                .email("admin@test.com")
+                .password("1234")
+                .build();
+
+        String requestBody = objectMapper.writeValueAsString(loginRequestDto);
+
+        mockMvc.perform(post("/api/login")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+}
