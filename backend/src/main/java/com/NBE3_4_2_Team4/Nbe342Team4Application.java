ea9692126d462ad8def2cfa66803faca4635f55f
sungjae <dtc13579@gmail.com>
2025-01-28 19:58:12 +0900

    fix: @EnableJpaAuditing 중복 코드 제거

--- backend/src/main/java/com/NBE3_4_2_Team4/Nbe342Team4Application.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/Nbe342Team4Application.java
@@ -2,10 +2,8 @@ package com.NBE3_4_2_Team4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class Nbe342Team4Application {
 
 	public static void main(String[] args) {
