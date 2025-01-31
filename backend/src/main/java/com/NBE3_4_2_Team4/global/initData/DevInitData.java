965e0654afac9feda6e0928738a98a847c499484
solji <solji.choi89@gmail.com>
2025-01-31 15:16:02 +0900

    SCRUM-93 feat- 스키마 파일 제작

---
+++ backend/src/main/java/com/NBE3_4_2_Team4/global/initData/DevInitData.java
@@ -0,0 +1,29 @@
+package com.NBE3_4_2_Team4.global.initData;
+
+import com.NBE3_4_2_Team4.standard.util.Ut;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
+
+@Profile("dev")
+@Configuration
+@RequiredArgsConstructor
+public class DevInitData {
+    @Autowired
+    @Lazy
+    private DevInitData self;
+
+    @Bean
+    public ApplicationRunner devInitDataApplicationRunner() {
+        return args -> {
+            Ut.file.downloadByHttp("http://localhost:8080/v3/api-docs", ".");
+
+            String cmd = "yes | npx --package typescript --package openapi-typescript openapi-typescript api-docs.json -o ../frontend/src/lib/backend/apiV1/schema.d.ts";
+            Ut.cmd.runAsync(cmd);
+        };
+    }
+}
