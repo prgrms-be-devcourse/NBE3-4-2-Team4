3e2ae6de67c876e6e7d79795bcd219e038070b4f
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2025-01-31 17:32:12 +0900

    SCRUM-90 feat: 질문 author 필드 추가

--- backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/entity/Question.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/entity/Question.java
@@ -1,8 +1,10 @@
 package com.NBE3_4_2_Team4.domain.board.question.entity;
 
+import com.NBE3_4_2_Team4.domain.member.member.entity.Member;
 import com.NBE3_4_2_Team4.global.jpa.entity.BaseTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 @Entity
@@ -12,6 +14,9 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class Question extends BaseTime {
+    @ManyToOne
+    private Member author;
+
     @Column(length = 100)
     private String title;
 
