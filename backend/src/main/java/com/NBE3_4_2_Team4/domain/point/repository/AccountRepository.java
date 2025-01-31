b76c562a005aaedf981d02dd2509ec78d3debf30
haeminK <khm65426542@gmail.com>
2025-01-31 22:45:37 +0900

    feature: 포인트 송금과 기록코드 작성

---
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/point/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.NBE3_4_2_Team4.domain.point.repository;
+
+
+import com.NBE3_4_2_Team4.domain.point.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+}
