30a46d699403639d34fb130794b9007c52a1acc5
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2025-02-03 15:15:30 +0900

    SCRUM-99 feat: 추천 테이블 생성

---
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/board/recommend/repository/RecommendRepository.java
@@ -0,0 +1,7 @@
+package com.NBE3_4_2_Team4.domain.board.recommend.repository;
+
+import com.NBE3_4_2_Team4.domain.board.recommend.entity.Recommend;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecommendRepository extends JpaRepository<Recommend, Long> {
+}
---
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/board/recommend/service/RecommendService.java
@@ -0,0 +1,4 @@
+package com.NBE3_4_2_Team4.domain.board.recommend.service;
+
+public class RecommendService {
+}
