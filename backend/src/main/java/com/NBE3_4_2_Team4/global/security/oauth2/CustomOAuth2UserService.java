e7c5643b0df3c566fd52e7abd6a751eae4e696fa
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-02-04 12:48:13 +0900

    refactor- 개발용으로 작성한 error 로그 제거

--- backend/src/main/java/com/NBE3_4_2_Team4/global/security/oauth2/CustomOAuth2UserService.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/global/security/oauth2/CustomOAuth2UserService.java
@@ -6,17 +6,14 @@ import com.NBE3_4_2_Team4.global.security.user.CustomUser;
 import com.NBE3_4_2_Team4.domain.member.member.entity.Member;
 import com.NBE3_4_2_Team4.domain.member.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
 
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
@@ -26,22 +23,23 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
+
         String providerTypeCode = userRequest
                 .getClientRegistration()
                 .getRegistrationId()
                 .toUpperCase();
-        Map<String, Object> attributes = oAuth2User.getAttributes();
-        log.warn("attributes: {}", attributes);
+
         Member.OAuth2Provider oAuth2Provider = Member.OAuth2Provider.getOAuth2ProviderByName(providerTypeCode);
+
         OAuth2UserInfo oAuth2UserInfo = oAuth2UserInfoFactory
                 .getOAuth2UserInfo
                         (oAuth2Provider, oAuth2User);
+
         String oAuth2Id = oAuth2UserInfo.getOAuth2Id();
         String nickname = oAuth2UserInfo.getNickname();
         String username = String.format("%s_%s", providerTypeCode, oAuth2Id);
 
         Member member = memberService.signUpOrModify(username, "", nickname, oAuth2Provider);
-        log.error("member: {}", member);
         return new CustomUser(member);
     }
 }
