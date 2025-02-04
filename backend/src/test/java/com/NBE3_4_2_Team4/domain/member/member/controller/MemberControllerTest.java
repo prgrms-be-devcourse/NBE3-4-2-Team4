3e65f7ca35d98efc26546ff089f7dd94a5412ce3
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-02-04 12:39:28 +0900

    remove- 삭제한 매서드에 따른 테스트 코드 삭제

--- backend/src/test/java/com/NBE3_4_2_Team4/domain/member/member/controller/MemberControllerTest.java
+++ backend/src/test/java/com/NBE3_4_2_Team4/domain/member/member/controller/MemberControllerTest.java
@@ -1,20 +1,15 @@
 package com.NBE3_4_2_Team4.domain.member.member.controller;
 
-import com.NBE3_4_2_Team4.domain.member.dto.request.LoginRequestDto;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -25,61 +20,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class MemberControllerTest {
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Test
-    void loginSuccessTest() throws Exception {
-        LoginRequestDto loginRequestDto = LoginRequestDto.builder()
-                .email("admin@test.com")
-                .password("1234")
-                .build();
-
-        String requestBody = objectMapper.writeValueAsString(loginRequestDto);
-
-        mockMvc.perform(post("/api/login")
-                        .with(csrf())
-                        .content(requestBody)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());
-    }
-
-    @Test
-    void loginFailureTest1() throws Exception {
-        LoginRequestDto loginRequestDto = LoginRequestDto.builder()
-                .email("wrong@test.com")
-                .password("1234")
-                .build();
-
-        String requestBody = objectMapper.writeValueAsString(loginRequestDto);
-
-        mockMvc.perform(post("/api/login")
-                        .with(csrf())
-                        .content(requestBody)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound())
-                .andDo(print());
-    }
-
-    @Test
-    void loginFailureTest2() throws Exception {
-        LoginRequestDto loginRequestDto = LoginRequestDto.builder()
-                .email("admin@test.com")
-                .password("wrong password")
-                .build();
-
-        String requestBody = objectMapper.writeValueAsString(loginRequestDto);
-
-        mockMvc.perform(post("/api/login")
-                        .with(csrf())
-                        .content(requestBody)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest())
-                .andDo(print());
-    }
-
 
     @Test
     @DisplayName("로그아웃 실패 테스트 - 로그아웃 요청이 아닌 직접 로그아웃 완료 페이지로 접근 시")
