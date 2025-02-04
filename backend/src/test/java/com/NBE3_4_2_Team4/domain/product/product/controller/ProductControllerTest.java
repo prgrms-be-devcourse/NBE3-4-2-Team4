48e97c846b440cd9d7529008d05821927519e298
sungjae <dtc13579@gmail.com>
2025-02-04 15:04:46 +0900

    test: 판매 상품별 상품 조회 API 테스트 케이스 추가 (일반, 페이징)

--- backend/src/test/java/com/NBE3_4_2_Team4/domain/product/product/controller/ProductControllerTest.java
+++ backend/src/test/java/com/NBE3_4_2_Team4/domain/product/product/controller/ProductControllerTest.java
@@ -169,4 +169,78 @@ class ProductControllerTest {
                 .andExpect(jsonPath("$.data.items[0].product_categories").value("기프티콘/음료/커피"))
                 .andExpect(jsonPath("$.data.items[0].product_sale_state").value("SOLDOUT"));
     }
+
+    @DisplayName("판매 상태별 상품 조회 Test")
+    @Test
+    void getProductsBySaleStateTest() throws Exception {
+
+        // given
+        String url = "/api/products/state/all?sale_state_keyword=SOLDOUT";
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                get(url)
+        ).andDo(print());
+
+        // then
+        resultActions
+                .andExpect(handler().handlerType(ProductController.class))
+                .andExpect(handler().methodName("getProductsByCategory"))
+                .andExpect(status().isOk())
+                // 첫번째 data 정합성 체크
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$.data.keyword").value("SOLDOUT"))
+                .andExpect(jsonPath("$.data.products").isArray())
+                .andExpect(jsonPath("$.data.products[0].product_id").value(1))
+                .andExpect(jsonPath("$.data.products[0].product_name").value("스타벅스 세트1"))
+                .andExpect(jsonPath("$.data.products[0].product_price").value(10000))
+                .andExpect(jsonPath("$.data.products[0].product_description").value("스타벅스 쿠폰1입니다."))
+                .andExpect(jsonPath("$.data.products[0].product_image_url").value("http://example.com/path/image1.jpg"))
+                .andExpect(jsonPath("$.data.products[0].product_categories").value("기프티콘/음료/커피"))
+                .andExpect(jsonPath("$.data.products[0].product_sale_state").value("SOLDOUT"))
+                // 두번째 data 정합성 체크
+                .andExpect(jsonPath("$.data.products[1].product_id").value(2))
+                .andExpect(jsonPath("$.data.products[1].product_name").value("스타벅스 세트2"))
+                .andExpect(jsonPath("$.data.products[1].product_price").value(15000))
+                .andExpect(jsonPath("$.data.products[1].product_description").value("스타벅스 쿠폰2입니다."))
+                .andExpect(jsonPath("$.data.products[1].product_image_url").value("http://example.com/path/image2.jpg"))
+                .andExpect(jsonPath("$.data.products[1].product_categories").value("기프티콘/음료/커피"))
+                .andExpect(jsonPath("$.data.products[1].product_sale_state").value("SOLDOUT"));
+    }
+
+    @DisplayName("판매 상태별 페이징 처리 된 상품 조회 Test")
+    @Test
+    void getProductsBySaleStateWithPagingTest() throws Exception {
+
+        // given
+        String url = "/api/products/state/all?sale_state_keyword=SOLDOUT&page=1&pageSize=1";
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                get(url)
+        ).andDo(print());
+
+        // then
+        resultActions
+                .andExpect(handler().handlerType(ProductController.class))
+                .andExpect(handler().methodName("getProductsByCategoryWithPaging"))
+                .andExpect(status().isOk())
+                // 페이지 정보 정합성 체크
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$.data.keyword").value("SOLDOUT"))
+                .andExpect(jsonPath("$.data.current_page_number").value(1))
+                .andExpect(jsonPath("$.data.page_size").value(1))
+                .andExpect(jsonPath("$.data.total_pages").value(2))
+                .andExpect(jsonPath("$.data.total_items").value(2))
+                .andExpect(jsonPath("$.data.has_more").value(true))
+                // 데이터 정합성 체크
+                .andExpect(jsonPath("$.data.items").isArray())
+                .andExpect(jsonPath("$.data.items[0].product_id").value(2))
+                .andExpect(jsonPath("$.data.items[0].product_name").value("스타벅스 세트2"))
+                .andExpect(jsonPath("$.data.items[0].product_price").value(15000))
+                .andExpect(jsonPath("$.data.items[0].product_description").value("스타벅스 쿠폰2입니다."))
+                .andExpect(jsonPath("$.data.items[0].product_image_url").value("http://example.com/path/image2.jpg"))
+                .andExpect(jsonPath("$.data.items[0].product_categories").value("기프티콘/음료/커피"))
+                .andExpect(jsonPath("$.data.items[0].product_sale_state").value("SOLDOUT"));
+    }
 }
