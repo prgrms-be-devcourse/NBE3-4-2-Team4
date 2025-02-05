dd3f13514b765a0121ff82639b9b13b1dab7f6fc
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-02-05 12:24:26 +0900

    feat- 카카오 연결 끊기 서비스 클래스 추가 (클래스만)

---
+++ backend/src/main/java/com/NBE3_4_2_Team4/global/security/oauth2/disconect/service/impl/KaKaoDisconnectService.java
@@ -0,0 +1,8 @@
+package com.NBE3_4_2_Team4.global.security.oauth2.disconect.service.impl;
+
+import com.NBE3_4_2_Team4.global.security.oauth2.disconect.service.OAuth2DisconnectService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KaKaoDisconnectService implements OAuth2DisconnectService {
+}
