0f7eea777c60f536d9c33cca89507bbfbdc6abcd
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-02-05 14:26:12 +0900

    feat- 토큰 관련 인터페이스/클래스 추가 (연결 끊기 시 사용할 예정)

---
+++ backend/src/main/java/com/NBE3_4_2_Team4/global/security/oauth2/token/service/impl/KakaoTokenService.java
@@ -0,0 +1,8 @@
+package com.NBE3_4_2_Team4.global.security.oauth2.token.service.impl;
+
+import com.NBE3_4_2_Team4.global.security.oauth2.token.service.OAuth2TokenService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KakaoTokenService implements OAuth2TokenService {
+}
