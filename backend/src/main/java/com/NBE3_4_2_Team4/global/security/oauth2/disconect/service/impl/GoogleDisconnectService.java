b0f49ad6ffff0b147a0e4ed211d7d27006344ead
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-02-06 12:36:36 +0900

    feat- 구글 엑세스 토큰 재발급 로직 구현

--- backend/src/main/java/com/NBE3_4_2_Team4/global/security/oauth2/disconect/service/impl/GoogleDisconnectService.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/global/security/oauth2/disconect/service/impl/GoogleDisconnectService.java
@@ -2,10 +2,14 @@ package com.NBE3_4_2_Team4.global.security.oauth2.disconect.service.impl;
 
 import com.NBE3_4_2_Team4.domain.member.member.entity.Member;
 import com.NBE3_4_2_Team4.global.security.oauth2.disconect.service.OAuth2DisconnectService;
+import com.NBE3_4_2_Team4.global.security.oauth2.token.service.impl.GoogleTokenService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class GoogleDisconnectService implements OAuth2DisconnectService {
+    private final GoogleTokenService googleTokenService;
 
     @Override
     public Member.OAuth2Provider getProvider() {
@@ -14,6 +18,7 @@ public class GoogleDisconnectService implements OAuth2DisconnectService {
 
     @Override
     public boolean disconnect(String refreshToken) {
+        String accessToken = googleTokenService.getFreshAccessToken(refreshToken);
         return true;
     }
 }
--- backend/src/main/java/com/NBE3_4_2_Team4/global/security/oauth2/token/service/impl/GoogleTokenService.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/global/security/oauth2/token/service/impl/GoogleTokenService.java
@@ -1,12 +1,53 @@
 package com.NBE3_4_2_Team4.global.security.oauth2.token.service.impl;
 
 import com.NBE3_4_2_Team4.global.security.oauth2.token.service.OAuth2TokenService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
+@RequiredArgsConstructor
 public class GoogleTokenService implements OAuth2TokenService {
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
+
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public String getFreshAccessToken(String refreshToken) {
-        return "";
+        String googleTokenUrl = "https://oauth2.googleapis.com/token";
+
+        String url = UriComponentsBuilder.fromUriString(googleTokenUrl)
+                .queryParam("grant_type", "refresh_token")
+                .queryParam("client_id", clientId)
+                .queryParam("client_secret", clientSecret)
+                .queryParam("refresh_token", refreshToken)
+                .toUriString();
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.postForEntity(url, requestEntity, String.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            try {
+                return objectMapper.readTree(response.getBody())
+                        .get("access_token").asText();
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException("Failed to parse access token", e);
+            }
+        }else {
+            throw new RuntimeException("Failed to parse access token");
+        }
     }
 }
