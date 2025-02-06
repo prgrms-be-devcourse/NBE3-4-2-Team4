f7e5e006918500ae7218c0096291e014de248db5
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-02-06 11:43:01 +0900

    refactor- userService에서 OAuth2RefreshToken을 저장하는 방식을 redis -> DB로 변경

--- backend/src/main/java/com/NBE3_4_2_Team4/domain/member/OAuth2RefreshToken/entity/OAuth2RefreshToken.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/member/OAuth2RefreshToken/entity/OAuth2RefreshToken.java
@@ -2,8 +2,14 @@ package com.NBE3_4_2_Team4.domain.member.OAuth2RefreshToken.entity;
 
 import com.NBE3_4_2_Team4.domain.member.member.entity.Member;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OAuth2RefreshToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- backend/src/main/java/com/NBE3_4_2_Team4/global/security/oauth2/CustomOAuth2UserService.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/global/security/oauth2/CustomOAuth2UserService.java
@@ -1,5 +1,7 @@
 package com.NBE3_4_2_Team4.global.security.oauth2;
 
+import com.NBE3_4_2_Team4.domain.member.OAuth2RefreshToken.entity.OAuth2RefreshToken;
+import com.NBE3_4_2_Team4.domain.member.OAuth2RefreshToken.repository.OAuth2RefreshTokenRepository;
 import com.NBE3_4_2_Team4.global.security.oauth2.userInfo.OAuth2UserInfoFactory;
 import com.NBE3_4_2_Team4.global.security.oauth2.userInfo.OAuth2UserInfo;
 import com.NBE3_4_2_Team4.global.security.user.CustomUser;
@@ -7,15 +9,11 @@ import com.NBE3_4_2_Team4.domain.member.member.entity.Member;
 import com.NBE3_4_2_Team4.domain.member.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
-
-import java.util.concurrent.TimeUnit;
-
 
 @Slf4j
 @Component
@@ -23,11 +21,12 @@ import java.util.concurrent.TimeUnit;
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private final MemberService memberService;
     private final OAuth2UserInfoFactory oAuth2UserInfoFactory;
-    private final RedisTemplate<String, String> redisTemplate;
+    private final OAuth2RefreshTokenRepository oAuth2RefreshTokenRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        String refreshToken = userRequest.getAdditionalParameters().get("refresh_token").toString();
+        String refreshToken = (String) userRequest.getAdditionalParameters().getOrDefault("refresh_token", null);
+
         OAuth2User oAuth2User = super.loadUser(userRequest);
         String providerTypeCode = userRequest
                 .getClientRegistration()
@@ -44,12 +43,17 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         String nickname = oAuth2UserInfo.getNickname();
         String username = String.format("%s_%s", providerTypeCode, oAuth2Id);
 
-        if (refreshToken != null && !refreshToken.isBlank()){
-            redisTemplate.opsForValue().set(username, refreshToken, 1, TimeUnit.DAYS);
-        }
-
         Member member = memberService.signUpOrModify(username, "", nickname, oAuth2Provider);
         log.info("member: {}", member);
+
+        if (refreshToken != null && !refreshToken.isBlank()) {
+            OAuth2RefreshToken oAuth2RefreshToken = OAuth2RefreshToken.builder()
+                    .member(member)
+                    .refreshToken(refreshToken)
+                    .build();
+            oAuth2RefreshTokenRepository.save(oAuth2RefreshToken);
+        }
+
         return new CustomUser(member);
     }
 }
