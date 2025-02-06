ea93cab18ac6015abe120bba33a580f1a5335bce
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-02-06 10:11:15 +0900

    refactor- OAuth2LogoutServiceFactoryConfig 삭제로 참조 못하게 된 logoutCompleteUrl을 OAuth2LogoutService에 할당

--- backend/src/main/java/com/NBE3_4_2_Team4/global/security/oauth2/logout/service/OAuth2LogoutService.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/global/security/oauth2/logout/service/OAuth2LogoutService.java
@@ -2,7 +2,6 @@ package com.NBE3_4_2_Team4.global.security.oauth2.logout.service;
 
 
 import com.NBE3_4_2_Team4.domain.member.member.entity.Member;
-import com.NBE3_4_2_Team4.global.config.OAuth2LogoutFactoryConfig;
 import org.springframework.beans.factory.annotation.Value;
 
 public abstract class OAuth2LogoutService {
@@ -12,7 +11,8 @@ public abstract class OAuth2LogoutService {
     public abstract Member.OAuth2Provider getOAuth2Provider();
 
     public String getLogoutRedirectUrl() {
-        return backendDomain + OAuth2LogoutFactoryConfig.LOGOUT_COMPLETE_URL;
+        String logoutCompleteUrl = "/api/logout/complete";
+        return backendDomain + logoutCompleteUrl;
     }
 
     public String getLogoutUrl(){
