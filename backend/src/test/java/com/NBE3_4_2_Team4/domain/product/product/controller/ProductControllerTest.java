cab35607ea71faa797687863a0179ae30a61e274
sungjae <dtc13579@gmail.com>
2025-02-06 17:12:43 +0900

    test: 상품 CRUD API 테스트 케이스 추가

--- backend/src/test/java/com/NBE3_4_2_Team4/domain/product/product/controller/ProductControllerTest.java
+++ backend/src/test/java/com/NBE3_4_2_Team4/domain/product/product/controller/ProductControllerTest.java
@@ -1,11 +1,14 @@
 package com.NBE3_4_2_Team4.domain.product.product.controller;
 
-import com.NBE3_4_2_Team4.domain.product.product.service.ProductService;
+import com.NBE3_4_2_Team4.domain.product.product.dto.ProductRequestDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -20,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ProductControllerTest {
 
     @Autowired
-    private ProductService productService;
+    private ObjectMapper objectMapper;
 
     @Autowired
     private MockMvc mockMvc;
@@ -229,5 +232,124 @@ class ProductControllerTest {
                 .andExpect(jsonPath("$.data.items[0].product_image_url").value("http://example.com/path/chicken.jpg"))
                 .andExpect(jsonPath("$.data.items[0].product_category").value("치킨/BHC"))
                 .andExpect(jsonPath("$.data.items[0].product_sale_state").value("ONSALE"));
+    }
+
+    @DisplayName("단건 상품 조회 Test")
+    @Test
+    void getProductTest() throws Exception {
+
+        // given
+        String url = "/api/products/1";
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                get(url)
+        ).andDo(print());
+
+        // then
+        resultActions
+                .andExpect(handler().handlerType(ProductController.class))
+                .andExpect(handler().methodName("getProduct"))
+                .andExpect(status().isOk())
+                // 데이터 정합성 체크
+                .andExpect(jsonPath("$.data.product_id").value(1))
+                .andExpect(jsonPath("$.data.product_name").value("딸기라떼"))
+                .andExpect(jsonPath("$.data.product_price").value(3800))
+                .andExpect(jsonPath("$.data.product_description").value("빽다방 딸기라떼입니다."))
+                .andExpect(jsonPath("$.data.product_image_url").value("http://example.com/path/pack1.jpg"))
+                .andExpect(jsonPath("$.data.product_category").value("카페/빽다방"))
+                .andExpect(jsonPath("$.data.product_sale_state").value("ONSALE"));
+    }
+
+    @DisplayName("단건 상품 생성 Test")
+    @WithUserDetails("admin@test.com")
+    @Test
+    void writeProductTest() throws Exception {
+
+        // given
+        String url = "/api/products";
+        ProductRequestDto.writeItem saved = ProductRequestDto.writeItem.builder()
+                .productName("테스트용 상품")
+                .productPrice(1)
+                .productDescription("테스트용 상품입니다.")
+                .productImageUrl("http://example.com/path/test.jpg")
+                .productCategory("테스트")
+                .productSaleState("SOLDOUT")
+                .build();
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(saved))
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andDo(print());
+
+        // then
+        resultActions
+                .andExpect(handler().handlerType(ProductController.class))
+                .andExpect(handler().methodName("writeProduct"))
+                .andExpect(status().isOk())
+                // 데이터 정합성 체크
+                .andExpect(jsonPath("$.data.product_id").value(6));
+    }
+
+    @DisplayName("단건 상품 수정 Test")
+    @WithUserDetails("admin@test.com")
+    @Test
+    void updateProductTest() throws Exception {
+
+        // given
+        String url = "/api/products/5";
+        ProductRequestDto.updateItem updated = new ProductRequestDto.updateItem(
+                "테스트용 상품 (수정)",
+                2,
+                "테스트용 상품 수정본 입니다.",
+                "http://example.com/path/test-update.jpg",
+                "테스트/수정",
+                "COMINGSOON"
+        );
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                patch(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updated))
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andDo(print());
+
+        // then
+        resultActions
+                .andExpect(handler().handlerType(ProductController.class))
+                .andExpect(handler().methodName("updateProduct"))
+                .andExpect(status().isOk())
+                // 데이터 정합성 체크
+                .andExpect(jsonPath("$.data.product_id").value(5))
+                .andExpect(jsonPath("$.data.product_name").value("테스트용 상품 (수정)"))
+                .andExpect(jsonPath("$.data.product_price").value(2))
+                .andExpect(jsonPath("$.data.product_description").value("테스트용 상품 수정본 입니다."))
+                .andExpect(jsonPath("$.data.product_image_url").value("http://example.com/path/test-update.jpg"))
+                .andExpect(jsonPath("$.data.product_category").value("테스트/수정"))
+                .andExpect(jsonPath("$.data.product_sale_state").value("COMINGSOON"));
+    }
+
+    @DisplayName("단건 상품 삭제 Test")
+    @WithUserDetails("admin@test.com")
+    @Test
+    void deleteProductTest() throws Exception {
+
+        // given
+        String url = "/api/products/6";
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                delete(url)
+        ).andDo(print());
+
+        // then
+        resultActions
+                .andExpect(handler().handlerType(ProductController.class))
+                .andExpect(handler().methodName("deleteProduct"))
+                .andExpect(status().isOk());
     }
 }
