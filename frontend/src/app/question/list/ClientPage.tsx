38c884f02780475c4d46236c16571a6755b820da
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2025-02-08 22:24:19 +0900

    SCRUM-123 feat: 리스트에 답변 수, 추천 수 추가

--- frontend/src/app/question/list/ClientPage.tsx
+++ frontend/src/app/question/list/ClientPage.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import type { components } from "@/lib/backend/apiV1/schema";
 import { formatDate } from "@/utils/dateUtils";
+import { MessageCircle } from "lucide-react";
+import { ThumbsUp } from "lucide-react";
 import Link from "next/link";
 
 type QuestionDto = components["schemas"]["QuestionDto"];
@@ -70,7 +72,21 @@ export default function ClientPage({ body }: ClientPageProps) {
           className="flex items-center dark:bg-gray-800 justify-between border-2 border-gray-300 p-3 rounded-md">
             <Link href={`/question/${item.id}`} className="flex items-center justify-between w-full block">
               <div className="flex-1 font-semibold text-lg truncate">{item.title}</div>
-              <div className="w-40 text-sm text-center">{item.name}</div>
+              <div className="w-40 text-sm text-center flex items-center justify-between">
+              {item.recommendCount && item.recommendCount > 0 ? (
+                <span className="flex items-center gap-1 text-purple-500 font-medium">
+                  <ThumbsUp size={16} />
+                  {item.recommendCount}
+                </span>
+              ) : <span></span>}
+              {item.answers && item.answers?.length > 0 ? (
+                <span className="flex items-center gap-1 text-blue-500 font-medium">
+                  <MessageCircle size={16} />
+                  {item.answers?.length}
+                </span>
+              ) : <span></span>}
+              <span>{item.name}</span>
+              </div>
               <div className="w-56 text-sm text-right">작성 일시: {formatDate(item.createdAt)}</div><br />
             </Link>
           </li>
@@ -95,8 +111,8 @@ export default function ClientPage({ body }: ClientPageProps) {
           <button key={page} onClick={() => changePage(page)}
             className={`px-3 py-2 rounded-md text-white font-semibold transition ${
               currentPage === page
-                ? "bg-cyan-100 cursor-not-allowed" // 현재 페이지 비활성화
-                : "bg-cyan-500 hover:bg-cyan-600"
+                ? "bg-blue-100 cursor-not-allowed" // 현재 페이지 비활성화
+                : "bg-blue-300 hover:bg-blue-500"
             }`}
             >
             {page}
