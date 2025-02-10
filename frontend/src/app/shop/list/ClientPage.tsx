b3b8b7292f75b09a52a83212945fc33f75bfc029
sungjae <dtc13579@gmail.com>
2025-02-10 10:41:36 +0900

    feat: 상품 메인 화면 추가 (상품 리스트 추가)

--- frontend/src/app/shop/list/ClientPage.tsx
+++ frontend/src/app/shop/list/ClientPage.tsx
@@ -1,5 +1,29 @@
 "use client";
 
-export default function ClientPage() {
-  return <div className="container mx-auto px-4">상품 리스트</div>;
+import type { components } from "@/lib/backend/apiV1/schema";
+import PostList from "@/lib/business/components/PostList";
+
+export default function ClientPage({
+                                     page,
+                                     pageSize,
+                                     itemPage,
+                                   }: {
+  page: number;
+  pageSize: number;
+  itemPage: components["schemas"]["PageDtoGetItem"];
+}) {
+
+  return (
+      <>
+        <div className="container mx-auto px-4">
+          <h1 className="text-2xl font-bold text-center my-4">상품 리스트</h1>
+
+          <PostList
+              page={page}
+              pageSize={pageSize}
+              itemPage={itemPage}
+          />
+        </div>
+      </>
+  );
 }
