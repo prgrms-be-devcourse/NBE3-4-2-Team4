b162668b30f264904e055f8a3f183fd2db3fdf44
solji <solji.choi89@gmail.com>
2025-02-12 05:18:53 +0900

    refactor: 포인트 쇼핑 스타일 완료

--- frontend/src/app/shop/[product_id]/ClientPage.tsx
+++ frontend/src/app/shop/[product_id]/ClientPage.tsx
@@ -5,162 +5,192 @@ import { useRouter } from "next/navigation";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import type { paths } from "@/lib/backend/apiV1/schema";
-import createClient from "openapi-fetch";
 import { useState } from "react";
 import imageLoader from "@/utils/imageLoader";
-
-const client = createClient<paths>({
-    baseUrl: "http://localhost:8080",
-    credentials: "include",
-});
+import client from "@/lib/backend/client";
+import { useToast } from "@/hooks/use-toast";
 
 export default function ClientPage({ product, cookieString }) {
-    const router = useRouter();
-    const [loading, setLoading] = useState(false); // 로딩 상태 추가
+  const router = useRouter();
+  const [loading, setLoading] = useState(false); // 로딩 상태 추가
 
-    // 구매 버튼 클릭 시 API 요청
-    const onPurchase = async () => {
-        if (loading) return; // 이미 요청 중이면 중복 실행 방지
-        setLoading(true); // 로딩 시작
+  const { toast } = useToast();
 
-        try {
-            // 유저 정보 조회
-            const userResponse = await client.GET("/api/members/details", {
-                headers: {
-                    cookie: cookieString,
-                },
-            });
+  // 구매 버튼 클릭 시 API 요청
+  const onPurchase = async () => {
+    if (loading) return; // 이미 요청 중이면 중복 실행 방지
+    setLoading(true); // 로딩 시작
 
-            // 401 응답이면 로그인 페이지로 리디렉션
-            if (userResponse.response.status === 401) {
-                alert("로그인이 필요합니다.");
-                router.push("/login");
-                return;
-            }
+    try {
+      // 유저 정보 조회
+      const userResponse = await client.GET("/api/members/details", {
+        headers: {
+          cookie: cookieString,
+        },
+      });
 
-            console.log(userResponse);
+      // 401 응답이면 로그인 페이지로 리디렉션
+      if (userResponse.response.status === 401) {
+        //alert("로그인이 필요합니다.");
+        toast({
+          title: "로그인이 필요합니다.",
+          variant: "destructive",
+        });
 
-            const point = userResponse.data.data!.point;
-            const username = userResponse.data.data!.username;
+        router.push("/login");
+        return;
+      }
 
-            console.log(`유저 이름: ${username}, 현재 포인트: ${point}`);
+      console.log(userResponse);
 
-            // 포인트 확인: 부족하면 알림 후 중단
-            if (point < product.product_price) {
-                alert("상품을 구매할 포인트가 부족합니다.");
-                setLoading(false);
-                return;
-            }
+      const point = userResponse.data.data!.point;
+      const username = userResponse.data.data!.username;
 
-            // 포인트 차감 요청
-            const pointDeductResponse = await client.PUT("/api/order/{product_id}", {
-                params: {
-                    path: {
-                        product_id: product.product_id
-                    },
-                },
+      console.log(`유저 이름: ${username}, 현재 포인트: ${point}`);
 
-                body: {
-                    username: username,
-                    amount: product.product_price,
-                },
-            });
+      // 포인트 확인: 부족하면 알림 후 중단
+      if (point < product.product_price) {
+        //alert("상품을 구매할 포인트가 부족합니다.");
+        toast({
+          title: "상품을 구매할 포인트가 부족합니다.",
+          variant: "destructive",
+        });
+        setLoading(false);
+        return;
+      }
 
-            if (pointDeductResponse.error || !pointDeductResponse.data) {
-                throw new Error(`포인트 차감 실패: ${pointDeductResponse.status}`);
-            }
+      // 포인트 차감 요청
+      const pointDeductResponse = await client.PUT("/api/order/{product_id}", {
+        params: {
+          path: {
+            product_id: product.product_id,
+          },
+        },
 
-            console.log("포인트 차감 성공:", pointDeductResponse);
+        body: {
+          username: username,
+          amount: product.product_price,
+        },
+      });
 
-            //  구매 성공 후 알림 및 상품 리스트로 이동
-            alert("구매가 성공하였습니다.");
-            router.push(`/shop/list`);
-        } catch (error) {
-            console.error("구매 요청 중 오류:", error);
+      if (pointDeductResponse.error || !pointDeductResponse.data) {
+        //throw new Error(`포인트 차감 실패: ${pointDeductResponse.status}`);
+        toast({
+          title: "포인트 차감 실패",
+          description: pointDeductResponse.error.msg,
+          variant: "destructive",
+        });
 
-            if (error.message.includes("401") || error.response?.status === 401) {
-                router.push("/login");
-            } else if (error.message.includes("404") || error.response?.status === 404) {
-                alert("구매 요청에 실패하였습니다.");
-            } else {
-                alert("알 수 없는 오류가 발생했습니다.");
-            }
-        } finally {
-            setLoading(false); // 로딩 종료
-        }
-    };
+        setLoading(false);
+        return;
+      }
 
-    return (
-        <div className="container mx-auto px-4 py-8">
-            <div className="flex flex-col md:flex-row gap-6">
-                {/* 상품 상세 정보 */}
-                <div className="flex-1">
-                    <Card className="mb-6">
-                        <CardHeader>
-                            {product.product_image_url ? (
-                                <Image
-                                    loader={imageLoader}
-                                    src={product.product_image_url}
-                                    alt={product.product_name}
-                                    width={600}
-                                    height={400}
-                                    className="object-cover rounded-xl"
-                                />
-                            ) : (
-                                <div className="w-full h-64 bg-gray-200 rounded-xl flex items-center justify-center">
-                                    이미지 없음
-                                </div>
-                            )}
-                        </CardHeader>
-                        <CardContent>
-                            <CardTitle className="text-3xl font-bold mb-4">
-                                {product.product_name}
-                            </CardTitle>
-                            <p className="text-xl font-semibold text-primary mb-4">
-                                {product.product_price.toLocaleString()} 원
-                            </p>
-                        </CardContent>
-                    </Card>
+      console.log("포인트 차감 성공:", pointDeductResponse);
 
-                    {product.product_description && (
-                        <div className="mb-6 p-6 bg-white border rounded-xl shadow">
-                            <h3 className="text-2xl font-semibold mb-4">상품 설명</h3>
-                            <p className="text-base text-gray-700">{product.product_description}</p>
-                        </div>
-                    )}
-                </div>
+      //  구매 성공 후 알림 및 상품 리스트로 이동
+      toast({
+        title: "구매가 성공하였습니다.",
+      });
+      router.push(`/shop/list`);
+    } catch (error) {
+      console.error("구매 요청 중 오류:", error);
 
-                {/* 데스크톱용 구매 정보 (오른쪽 고정) */}
-                <div className="hidden md:block w-1/3">
-                    <div className="sticky top-20 h-screen">
-                        <Card className="h-full">
-                            <CardContent className="pt-6 h-full flex flex-col justify-between">
-                                <div className="space-y-4">
-                                    <h2 className="text-2xl font-bold mb-6">구매 정보</h2>
-                                    <div className="flex justify-between items-center">
-                                        <span className="text-lg">{product.product_name}</span>
-                                        <span className="text-lg font-semibold text-primary">
-                                            {product.product_price.toLocaleString()} 원
-                                        </span>
-                                    </div>
-                                </div>
-                                <div>
-                                    <hr />
-                                    <div className="flex justify-between items-center mt-4">
-                                        <span className="text-xl font-bold">총 결제 금액</span>
-                                        <span className="text-xl font-bold text-primary">
-                                            {product.product_price.toLocaleString()} 원
-                                        </span>
-                                    </div>
-                                    <Button onClick={onPurchase} disabled={loading} className="w-full mt-4">
-                                        {loading ? "구매 진행 중..." : "포인트로 구매하기"}
-                                    </Button>
-                                </div>
-                            </CardContent>
-                        </Card>
-                    </div>
-                </div>
+      if (error.message.includes("401") || error.response?.status === 401) {
+        router.push("/login");
+      } else if (
+        error.message.includes("404") ||
+        error.response?.status === 404
+      ) {
+        //alert("구매 요청에 실패하였습니다.");
+        toast({
+          title: "구매 요청에 실패하였습니다.",
+          variant: "destructive",
+        });
+      } else {
+        //alert("알 수 없는 오류가 발생했습니다.");
+        toast({
+          title: "알 수 없는 오류가 발생했습니다.",
+          variant: "destructive",
+        });
+      }
+    } finally {
+      setLoading(false); // 로딩 종료
+    }
+  };
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="mt-20 mb-10 text-center">
+        <h2 className="flex items-center text-4xl font-bold justify-center gap-2">
+          포인트 쇼핑
+        </h2>
+        <p className="text-md text-gray-400 mt-3">
+          적립한 포인트로 특별한 혜택을 누리세요
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row gap-6">
+        {/* 상품 상세 정보 */}
+        <div className="flex-1">
+          {product.product_image_url ? (
+            <Image
+              loader={imageLoader}
+              src={product.product_image_url}
+              alt={product.product_name}
+              width={600}
+              height={400}
+              className="object-cover w-full border border-gray-100"
+            />
+          ) : (
+            <div className="w-full h-64 bg-gray-200 rounded-xl flex items-center justify-center">
+              이미지 없음
             </div>
+          )}
+
+          <div className="text-3xl font-bold mt-6 mb-3">
+            {product.product_name}
+          </div>
+          <p className="text-xl font-semibold text-primary mb-4">
+            {product.product_price.toLocaleString()} 원
+          </p>
+
+          {product.product_description && (
+            <div className="mt-10 py-10 border-t border-dashed border-gray-200">
+              <h3 className="text-2xl font-semibold mb-4">상품 설명</h3>
+              <p className="text-base text-gray-700">
+                {product.product_description}
+              </p>
+            </div>
+          )}
         </div>
-    );
+
+        {/* 데스크톱용 구매 정보 (오른쪽 고정) */}
+        <div className="w-full md:w-1/3 lg:w-2/5">
+          <div className="sticky top-20">
+            <Card>
+              <CardContent className="pt-6 h-full flex flex-col justify-between">
+                <div className="space-y-4">
+                  <h2 className="text-2xl font-bold mb-6">구매 정보</h2>
+                </div>
+                <div>
+                  <div className="flex justify-between items-center">
+                    <span className="text-lg">{product.product_name}</span>
+                    <span className="text-4xl font-bold text-primary">
+                      {product.product_price.toLocaleString()} 원
+                    </span>
+                  </div>
+                  <Button
+                    onClick={onPurchase}
+                    disabled={loading}
+                    className="w-full mt-4"
+                  >
+                    {loading ? "구매 진행 중..." : "포인트로 구매하기"}
+                  </Button>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/src/app/shop/list/ClientPage.tsx
+++ frontend/src/app/shop/list/ClientPage.tsx
@@ -4,26 +4,28 @@ import type { components } from "@/lib/backend/apiV1/schema";
 import ProductList from "@/app/shop/list/ProductList";
 
 export default function ClientPage({
-                                     page,
-                                     pageSize,
-                                     itemPage,
-                                   }: {
+  page,
+  pageSize,
+  itemPage,
+}: {
   page: number;
   pageSize: number;
   itemPage: components["schemas"]["PageDtoGetItem"];
 }) {
-
   return (
-      <>
-        <div className="container mx-auto px-4">
-          <h1 className="text-2xl font-bold text-center my-4 mb-10">상품 리스트</h1>
-
-          <ProductList
-              page={page}
-              pageSize={pageSize}
-              itemPage={itemPage}
-          />
+    <>
+      <div className="container mx-auto px-4">
+        <div className="mt-20 mb-10 text-center">
+          <h2 className="flex items-center text-4xl font-bold justify-center gap-2">
+            포인트 쇼핑
+          </h2>
+          <p className="text-md text-gray-400 mt-3">
+            적립한 포인트로 특별한 혜택을 누리세요
+          </p>
         </div>
-      </>
+
+        <ProductList page={page} pageSize={pageSize} itemPage={itemPage} />
+      </div>
+    </>
   );
 }
