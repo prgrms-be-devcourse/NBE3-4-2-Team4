3df208fe1d756544a693491da855940247ff88e4
sungjae <dtc13579@gmail.com>
2025-02-11 12:50:58 +0900

    feat: 관리자 홈에서 상품 관리 페이지 링크 추가

--- frontend/src/app/adm/ClientPage.tsx
+++ frontend/src/app/adm/ClientPage.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import Link from "next/link";
+
 export default function ClientPage() {
   return (
-    <div className="flex-1 flex justify-center items-center">관리자 홈</div>
+      <div className="flex-1 flex flex-col justify-center items-center space-y-4">
+        <div>관리자 홈</div>
+        <Link href="/adm/products/list">상품 관리 페이지</Link>
+      </div>
   );
 }
