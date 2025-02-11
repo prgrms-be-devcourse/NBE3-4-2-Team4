a01e7b75ff30fb152e00989441a96bef78e594b5
haeminK <khm65426542@gmail.com>
2025-02-11 11:34:00 +0900

    관리자 페이지에 링크 추가

--- frontend/src/app/adm/ClientPage.tsx
+++ frontend/src/app/adm/ClientPage.tsx
@@ -6,11 +6,15 @@ import {useRedirectIfNotAuthenticated} from "@/lib/hooks/useRedirect";
 export default function ClientPage() {
   useRedirectIfNotAuthenticated();
   return (
-      <>
+      <div className="flex flex-col justify-center items-center gap-[10px]">
     <div className="flex-1 flex justify-center items-center">관리자 홈</div>
-  <a href="/point/admin" className="px-4 py-2 bg-blue-500 text-white rounded-lg">
-    포인트 관리 페이지로 이동
-  </a>
-  </>
+    <ul>
+        <li className="flex justify-center items-center">
+            <a href="/point/admin" className="underline">
+                포인트 관리 페이지로 이동
+              </a>
+        </li>
+    </ul>
+  </div>
   );
 }
