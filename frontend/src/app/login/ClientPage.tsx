603d6f0d95edef468010a048baa0bd234355ef24
solji <solji.choi89@gmail.com>
2025-02-12 11:22:03 +0900

    refactor: 사용자 로그인, 관리자 로그인 스타일 완료

--- frontend/src/app/login/ClientPage.tsx
+++ frontend/src/app/login/ClientPage.tsx
@@ -1,22 +1,50 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import {useRedirectIfAuthenticated} from "@/lib/hooks/useRedirect";  // 경로는 적절히 수정
+import { useRedirectIfAuthenticated } from "@/lib/hooks/useRedirect"; // 경로는 적절히 수정
+import { Globe, Mail, MessageCircle, Settings } from "lucide-react";
 
 export default function ClientPage() {
-    useRedirectIfAuthenticated();
+  useRedirectIfAuthenticated();
 
-    return (
-        <div className="flex-1 flex flex-col justify-center items-center p-4">
-            <h2 className="text-xl mb-4">로그인</h2>
-            <Link href="http://localhost:8080/oauth2/authorization/kakao">
-                <Button variant="link">카카오 로그인</Button>
-            </Link>
-            <Link href="http://localhost:8080/oauth2/authorization/naver">
-                <Button variant="link">네이버 로그인</Button>
-            </Link>
-            <Link href="http://localhost:8080/oauth2/authorization/google">
-                <Button variant="link">구글 로그인</Button>
-            </Link>
-        </div>
-    );
+  return (
+    <div className="flex-1 flex flex-col justify-center items-center p-4">
+      <Button
+        className="mb-2 w-[180px] h-[40px] bg-[#FEE500] hover:bg-[#FDD000] text-black"
+        asChild
+      >
+        <Link href="http://localhost:8080/oauth2/authorization/kakao">
+          <MessageCircle size={20} />
+          카카오 로그인
+        </Link>
+      </Button>
+      <Button
+        className="mb-2 w-[180px] h-[40px] bg-[#03C75A] hover:bg-[#02B150] text-white"
+        asChild
+      >
+        <Link href="http://localhost:8080/oauth2/authorization/naver">
+          <Mail size={20} />
+          네이버 로그인
+        </Link>
+      </Button>
+      <Button
+        className="mb-16 bg-[#4285F4] w-[180px] h-[40px] hover:bg-[#357ABD] text-white"
+        asChild
+      >
+        <Link href="http://localhost:8080/oauth2/authorization/google">
+          <Globe size={20} />
+          구글 로그인
+        </Link>
+      </Button>
+
+      <Button
+        className="bg-gray-800 w-[180px] h-[40px] hover:bg-gray-700 text-white"
+        asChild
+      >
+        <Link href="http://localhost:3000/adm/login">
+          <Settings size={20} />
+          관리자 로그인
+        </Link>
+      </Button>
+    </div>
+  );
 }
