fa4e77dee877693a54e67d0676342858439d4b17
solji <solji.choi89@gmail.com>
2025-02-12 21:34:57 +0900

    refactor: 관리자 - 포인트 관리 스타일 완료

--- frontend/src/app/adm/point/AccumulateForm.tsx
+++ frontend/src/app/adm/point/AccumulateForm.tsx
@@ -1,70 +1,95 @@
-"use client"
+"use client";
 import createClient from "openapi-fetch";
 import { useState } from "react";
-import { useRouter, usePathname } from 'next/navigation';
+import { useRouter, usePathname } from "next/navigation";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { paths } from "@/lib/backend/apiV1/schema";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/hooks/use-toast";
 
-    const client = createClient<paths>({
-      baseUrl: "http://localhost:8080",
-    });
+const client = createClient<paths>({
+  baseUrl: "http://localhost:8080",
+});
 
 export default function AccumulateForm() {
+  const [username, setUsername] = useState("");
+  const [amount, setAmount] = useState(0);
 
-    const [username, setUsername] = useState("");
-    const [amount, setAmount] = useState(0);
+  const router = useRouter();
+  const pathname = usePathname();
+  const { toast } = useToast();
 
-      const router = useRouter();
-      const pathname = usePathname();
-
-    const handleAccumulate = async(e) => {
-        console.log(amount);
-        if (!amount || amount < 0) {
-            alert("0보다 큰수를 입력해주세요");
-            e.preventDefault();
-            return;
-        }
-
-        if (username.trim() === "") {
-            alert("유저네임을 입력하세요.");
-            e.preventDefault();
-            return;
-        }
-
-
-        const data = await client.PUT("/api/admin/points/accumulate", {
-          body: {
-              username: username,
-              amount: Number(amount)
-          },
-      credentials: "include"
-        });
-
-        if (!data.response.ok){
-                    alert("실패: "+data.error.msg);
-                    return;
-                    }
-
-
-        alert("성공!!");
-        setUsername("");
-        setAmount(0);
-        router.replace(pathname);
+  const handleAccumulate = async (e) => {
+    console.log(amount);
+    if (!amount || amount < 0) {
+      //alert("0보다 큰수를 입력해주세요");
+      toast({
+        title: "0보다 큰수를 입력해주세요",
+        variant: "destructive",
+      });
+      e.preventDefault();
+      return;
     }
 
-    return <div>
-    <h1 className="mb-2">포인트 적립</h1>
-    <div style={{ display: "flex", justifyContent: "flex", gap: "1rem" }}>
+    if (username.trim() === "") {
+      //alert("유저네임을 입력하세요.");
+      toast({
+        title: "유저네임을 입력하세요.",
+        variant: "destructive",
+      });
+      e.preventDefault();
+      return;
+    }
 
-            <input type="text" placeholder="포인트 적립 대상"
-                   className="border-2 border-gray-300 px-2 rounded-md focus:outline-none focus:border-blue-500"
-                   value={username}
-                   onChange={(e) => setUsername(e.target.value)}/>
-            <input type="number" placeholder="포인트 금액을 입력하세요"
-                   className="border-2 border-gray-300 px-2 rounded-md focus:outline-none focus:border-blue-500"
-                   value={amount}
-                   onChange={(e) => setAmount(e.target.value)}/>
-            <button
-               className="border-2 border-blue-500 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
-               onClick={handleAccumulate}>
-                   적립</button>
-    </div>
-    </div>
+    const data = await client.PUT("/api/admin/points/accumulate", {
+      body: {
+        username: username,
+        amount: Number(amount),
+      },
+      credentials: "include",
+    });
+
+    if (!data.response.ok) {
+      //alert("실패: " + data.error.msg);
+      toast({
+        title: "적립 실패",
+        description: data.error?.msg,
+        variant: "destructive",
+      });
+      return;
+    }
+
+    //alert("성공!!");
+    toast({
+      title: "적립 성공!!",
+    });
+    setUsername("");
+    setAmount(0);
+    router.replace(pathname);
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-lg">포인트 적립</CardTitle>
+      </CardHeader>
+
+      <CardContent className="flex gap-2">
+        <Input
+          type="text"
+          placeholder="포인트 적립 대상"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input
+          type="number"
+          placeholder="포인트 금액을 입력하세요"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <Button onClick={handleAccumulate}>적립</Button>
+      </CardContent>
+    </Card>
+  );
+}
