2d89f538b3da485f307474e360822ac1b6feabd6
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-02-27 17:48:20 +0900

    feat- 관리자용 신고 서비스에 간단한 CRU 매서드 시그니처 생성 및 Report 클래스에 '처리된' 필드 추가

--- backend/src/main/java/com/NBE3_4_2_Team4/domain/report/report/entity/Report.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/report/report/entity/Report.java
@@ -5,9 +5,15 @@ import com.NBE3_4_2_Team4.domain.report.reportType.entity.ReportType;
 import com.NBE3_4_2_Team4.global.jpa.entity.BaseTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import lombok.*;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Report extends BaseTime {
+    @Setter
     @ManyToOne
     private ReportType reportType;
 
@@ -15,7 +21,11 @@ public class Report extends BaseTime {
     private Member reporter;
 
     @ManyToOne
-    private Member reported;
+    private Member reportedMember;
 
+    @Setter
     private String content;
+
+    @Setter
+    private boolean processed;
 }
--- backend/src/main/java/com/NBE3_4_2_Team4/domain/report/report/service/RepostServiceForAdmin.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/report/report/service/RepostServiceForAdmin.java
@@ -1,11 +1,25 @@
 package com.NBE3_4_2_Team4.domain.report.report.service;
 
+import com.NBE3_4_2_Team4.domain.report.report.entity.Report;
 import com.NBE3_4_2_Team4.domain.report.report.repository.ReportRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RepostServiceForAdmin {
     public ReportRepository reportRepository;
+
+    public Page<Report> findReportsByReporterId(Long reporterId) {
+        return null;
+    }
+
+    public Page<Report> findReportsByReportedMemberId(Long reportedMemberId) {
+        return null;
+    }
+
+    public void processReport(Report report) {
+        report.setProcessed(true);
+    }
 }
