03e679357cadabfe17ecd030d4645efa3bcf527c
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-02-27 17:37:08 +0900

    feat- 신고의 CRU 매서드 시그니처 생성 (Delete 제외)

--- backend/src/main/java/com/NBE3_4_2_Team4/domain/report/report/service/ReportServiceForMember.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/report/report/service/ReportServiceForMember.java
@@ -1,11 +1,23 @@
 package com.NBE3_4_2_Team4.domain.report.report.service;
 
+import com.NBE3_4_2_Team4.domain.report.report.entity.Report;
 import com.NBE3_4_2_Team4.domain.report.report.repository.ReportRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ReportServiceForMember {
     private final ReportRepository reportRepository;
+
+    public void saveNewReport() {}
+
+    public Page<Report> findReportsByReporterId(Long reporterId) {
+        return null;
+    }
+
+    public void updateReport() {}
 }
