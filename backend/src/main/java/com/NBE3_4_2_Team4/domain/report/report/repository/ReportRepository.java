087647529757c1942824218bd2bbf11fb14b035c
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-02-27 16:38:23 +0900

    feat- 신고 레포지터리, 서비스 클래스 추가

---
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/report/report/repository/ReportRepository.java
@@ -0,0 +1,7 @@
+package com.NBE3_4_2_Team4.domain.report.report.repository;
+
+import com.NBE3_4_2_Team4.domain.report.report.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+}
---
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/report/report/service/RepositoryService.java
@@ -0,0 +1,11 @@
+package com.NBE3_4_2_Team4.domain.report.report.service;
+
+import com.NBE3_4_2_Team4.domain.report.report.repository.ReportRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RepositoryService {
+    private final ReportRepository reportRepository;
+}
