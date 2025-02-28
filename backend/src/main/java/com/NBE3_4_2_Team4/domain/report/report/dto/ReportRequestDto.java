5c8daebd63c3b722b26cf9cbd5c4508b9fe5086d
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-02-28 16:06:49 +0900

    feat- 신고용 Dto 레코드 생성

---
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/report/report/dto/ReportRequestDto.java
@@ -0,0 +1,15 @@
+package com.NBE3_4_2_Team4.domain.report.report.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Builder;
+
+@Builder
+public record ReportRequestDto (
+        Long reportTypeId,
+        Long reporterId,
+        Long reportedId,
+        String content
+){
+    @JsonCreator
+    public ReportRequestDto{}
+}
---
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/report/reportType/repository/ReportTypeRepository.java
@@ -0,0 +1,7 @@
+package com.NBE3_4_2_Team4.domain.report.reportType.repository;
+
+import com.NBE3_4_2_Team4.domain.report.reportType.entity.ReportType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportTypeRepository extends JpaRepository<ReportType, Long> {
+}
