50a0f89f84eef73ea1c823648bcdb59db9278d3e
solji <solji.choi89@gmail.com>
2025-03-03 02:18:59 +0900

    chore: 빌드에 코틀린 추가, 어플리케이션 코틀린으로 변환

---
+++ backend/src/main/java/com/NBE3_4_2_Team4/Nbe342Team4Application.kt
@@ -0,0 +1,12 @@
+package com.NBE3_4_2_Team4
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Nbe342Team4Application
+
+fun main(args: Array<String>) {
+    runApplication<Nbe342Team4Application>(*args)
+}
