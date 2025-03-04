142474c83de52dca21f7bf8be2a25fe81e7f612f
solji <solji.choi89@gmail.com>
2025-03-05 02:20:26 +0900

    SCRUM-164 refactor: answer 파트 controller 변환

--- backend/src/main/java/com/NBE3_4_2_Team4/domain/board/answer/service/AnswerService.kt
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/board/answer/service/AnswerService.kt
@@ -2,14 +2,12 @@ package com.NBE3_4_2_Team4.domain.board.answer.service
 
 import com.NBE3_4_2_Team4.domain.board.answer.dto.AnswerDto
 import com.NBE3_4_2_Team4.domain.board.answer.entity.Answer
-import com.NBE3_4_2_Team4.domain.board.answer.entity.QAnswer.answer
 import com.NBE3_4_2_Team4.domain.board.answer.repository.AnswerRepository
 import com.NBE3_4_2_Team4.domain.board.question.entity.Question
 import com.NBE3_4_2_Team4.domain.board.question.repository.QuestionRepository
 import com.NBE3_4_2_Team4.domain.member.member.entity.Member
 import com.NBE3_4_2_Team4.global.exceptions.ServiceException
 import com.NBE3_4_2_Team4.global.security.AuthManager
-import lombok.RequiredArgsConstructor
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
@@ -18,7 +16,6 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@RequiredArgsConstructor
 class AnswerService(
     private val answerRepository: AnswerRepository,
     private val questionRepository: QuestionRepository
--- backend/src/main/java/com/NBE3_4_2_Team4/global/rsData/RsData.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/global/rsData/RsData.java
@@ -3,12 +3,10 @@ package com.NBE3_4_2_Team4.global.rsData;
 import com.NBE3_4_2_Team4.standard.base.Empty;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.lang.NonNull;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@AllArgsConstructor
 @Getter
 public class RsData<T> {
     public static final RsData<Empty> OK = new RsData<>("200-1", "OK", new Empty());
@@ -21,6 +19,12 @@ public class RsData<T> {
 
     @NonNull
     private final T data;
+
+    public RsData(String resultCode, String msg, T data) {
+        this.resultCode = resultCode;
+        this.msg = msg;
+        this.data = data;
+    }
 
     public RsData(String resultCode, String msg) {
         this(resultCode, msg, (T) new Empty());
