12d97bc96a5b787372f11e0bdf866f2eba36740e
solji <solji.choi89@gmail.com>
2025-03-05 09:20:30 +0900

    Refactor kotlin answer(scrum 164) (#49)
    * SCRUM-164 refactor: answer 빌더 제거
    
    * SCRUM-164 refactor: answer 엔티티 변환
    
    * SCRUM-164 refactor: answer 파트 dto 변환
    
    * SCRUM-164 refactor: answer 파트 initData 변환
    
    * SCRUM-164 refactor: answer 파트 repository, service 변환
    
    * SCRUM-164 refactor: answer 파트 controller 변환

--- backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/repository/QuestionRepositoryImpl.java
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/repository/QuestionRepositoryImpl.java
@@ -65,7 +65,7 @@ public class QuestionRepositoryImpl implements QuestionRepositoryCustom {
                 .selectOne()
                 .from(answer)
                 .where(answer.question.eq(question)
-                        .and(answer.content.containsIgnoreCase(kw)))
+                        .and(answer.answerContent.containsIgnoreCase(kw)))
                 .exists();
     }
 
