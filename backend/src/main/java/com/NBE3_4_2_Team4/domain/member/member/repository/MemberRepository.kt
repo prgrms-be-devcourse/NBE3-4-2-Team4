a325300b7e50a3d1dc65b5cad5c9686ff0633e3c
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-03-05 14:12:54 +0900

    refactor- MemberRepository, MemberService 코틀린화

--- backend/src/main/java/com/NBE3_4_2_Team4/domain/member/member/repository/MemberRepository.kt
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/member/member/repository/MemberRepository.kt
@@ -1,4 +1,23 @@
 package com.NBE3_4_2_Team4.domain.member.member.repository
 
-interface MemberRepository {
+import com.NBE3_4_2_Team4.domain.member.member.entity.Member
+import jakarta.persistence.LockModeType
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+
+
+interface MemberRepository : JpaRepository<Member, Long> {
+    fun findByUsername(username: String?): Member?
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT m FROM Member m WHERE m.username = :username")
+    fun findByUsernameWithLock(@Param("username") username: String?): Member?
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT m FROM Member m WHERE m.id = :id")
+    fun findByIdWithLock(@Param("id") id: Long?): Member?
+
+    fun existsByUsername(username: String?): Boolean
 }
