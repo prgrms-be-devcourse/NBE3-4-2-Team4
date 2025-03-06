ddf31bf7cd7c5cb02b6c2c722338932de29ff1b5
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2025-03-06 10:45:41 +0900

    refactor: Question 관련 service, repository 변환

--- backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/repository/QuestionCategoryRepository.kt
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/repository/QuestionCategoryRepository.kt
@@ -1,7 +1,6 @@
-package com.NBE3_4_2_Team4.domain.board.question.repository;
+package com.NBE3_4_2_Team4.domain.board.question.repository
 
-import com.NBE3_4_2_Team4.domain.board.question.entity.QuestionCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.NBE3_4_2_Team4.domain.board.question.entity.QuestionCategory
+import org.springframework.data.jpa.repository.JpaRepository
 
-public interface QuestionCategoryRepository extends JpaRepository<QuestionCategory, Long> {
-}
+interface QuestionCategoryRepository : JpaRepository<QuestionCategory, Long>
