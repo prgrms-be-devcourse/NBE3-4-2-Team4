83e0a73973f79168b941f9368390ccad410b7fd8
sungjae <dtc13579@gmail.com>
2025-03-06 17:56:59 +0900

    fix: 누락된 PointController 추가

---
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/asset/point/controller/PointController.java
@@ -0,0 +1,33 @@
+package com.NBE3_4_2_Team4.domain.asset.point.controller;
+
+import com.NBE3_4_2_Team4.domain.member.member.entity.Member;
+import com.NBE3_4_2_Team4.domain.asset.point.service.PointService;
+import com.NBE3_4_2_Team4.global.rsData.RsData;
+import com.NBE3_4_2_Team4.standard.base.Empty;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import static com.NBE3_4_2_Team4.global.security.AuthManager.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/points")
+@Tag(name = "PointController", description = "API 포인트 컨트롤러")
+public class PointController {
+    private final PointService pointService;
+
+    @PutMapping("/attendance")
+    @Operation(summary="출석요청", description="출석요청이 이미 완료이면 에러")
+    public RsData<Empty> attendance() {
+        Member member = getNonNullMember();
+        pointService.attend(member.getId());
+
+        return new RsData<>(
+                "200-1",
+                "출석 성공",
+                new Empty()
+        );
+    }
+}
