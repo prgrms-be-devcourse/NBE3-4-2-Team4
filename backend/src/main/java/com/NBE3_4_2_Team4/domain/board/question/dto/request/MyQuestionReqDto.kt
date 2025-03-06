d2e04d5caa1e34e357261b72d091a0e332fe71d2
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2025-03-06 12:22:08 +0900

    refactor: Question req/res dto, initData 변환

--- backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/dto/request/MyQuestionReqDto.kt
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/dto/request/MyQuestionReqDto.kt
@@ -2,7 +2,7 @@ package com.NBE3_4_2_Team4.domain.board.question.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 
-public record MyQuestionReqDto(
-        @NotNull
-        String username
+data class MyQuestionReqDto(
+        @field:NotNull
+        val username: String
 ) {}
--- backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/dto/request/QuestionWriteReqDto.kt
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/dto/request/QuestionWriteReqDto.kt
@@ -1,21 +1,29 @@
-package com.NBE3_4_2_Team4.domain.board.question.dto.request;
+package com.NBE3_4_2_Team4.domain.board.question.dto.request
 
-import com.NBE3_4_2_Team4.domain.asset.main.entity.AssetType;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.Length;
+import com.NBE3_4_2_Team4.domain.asset.main.entity.AssetType
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotNull
+import org.hibernate.validator.constraints.Length
 
-public record QuestionWriteReqDto(
-        @NotNull @Length(min = 2)
-        String title,
-        @NotNull @Length(min = 2)
-        String content,
-        @NotNull
-        @JsonProperty("categoryId")
-        Long categoryId,
-        @NotNull @Min(1)
-        long amount,
-        @NotNull @JsonProperty("assetType")
-        AssetType assetType
-) {}
+data class QuestionWriteReqDto(
+        @field:NotNull
+        @field:Length(min = 2)
+        val title: String,
+
+        @field:NotNull
+        @field:Length(min = 2)
+        val content: String,
+
+        @field:NotNull
+        @field:JsonProperty("categoryId")
+        val categoryId: Long,
+
+        @field:NotNull
+        @field:Min(1)
+        val amount: Long,
+
+        @field:NotNull
+        @field:JsonProperty("assetType")
+        val assetType: AssetType
+)
