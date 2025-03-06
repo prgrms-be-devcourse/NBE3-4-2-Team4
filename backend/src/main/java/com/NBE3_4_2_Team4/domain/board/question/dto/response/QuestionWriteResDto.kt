6336a25b31235bc3187ebb1314bff983609e6c10
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2025-03-06 12:22:08 +0900

    refactor: Question req/res dto, initData 변환

--- backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/dto/response/QuestionWriteResDto.kt
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/dto/response/QuestionWriteResDto.kt
@@ -2,7 +2,7 @@ package com.NBE3_4_2_Team4.domain.board.question.dto.response;
 
 import com.NBE3_4_2_Team4.domain.board.question.dto.QuestionDto;
 
-public record QuestionWriteResDto(
-        QuestionDto item,
-        long totalCount
-) {}
+data class QuestionWriteResDto(
+    val item: QuestionDto,
+    val totalCount: Long
+)
