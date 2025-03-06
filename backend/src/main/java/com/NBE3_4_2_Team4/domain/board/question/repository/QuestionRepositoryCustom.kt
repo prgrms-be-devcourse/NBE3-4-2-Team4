7bd052ede596b2b62d730c09820e0b54b1a418b2
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2025-03-06 12:05:54 +0900

    refactor: Question repository 변환(impl, custom)

--- backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/repository/QuestionRepositoryCustom.kt
+++ backend/src/main/java/com/NBE3_4_2_Team4/domain/board/question/repository/QuestionRepositoryCustom.kt
@@ -1,13 +1,12 @@
-package com.NBE3_4_2_Team4.domain.board.question.repository;
+package com.NBE3_4_2_Team4.domain.board.question.repository
 
+import com.NBE3_4_2_Team4.domain.board.question.entity.Question
+import com.NBE3_4_2_Team4.domain.member.member.entity.Member
+import com.NBE3_4_2_Team4.standard.search.QuestionSearchKeywordType
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 
-import com.NBE3_4_2_Team4.domain.board.question.entity.Question;
-import com.NBE3_4_2_Team4.domain.member.member.entity.Member;
-import com.NBE3_4_2_Team4.standard.search.QuestionSearchKeywordType;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface QuestionRepositoryCustom {
-    Page<Question> findByKw(QuestionSearchKeywordType kwType, String kw, Pageable pageable);
-    Page<Question> findByAnswerAuthor(Member author, Pageable pageable);
+interface QuestionRepositoryCustom {
+    fun findByKw(kwType: QuestionSearchKeywordType, kw: String, pageable: Pageable): Page<Question>
+    fun findByAnswerAuthor(author: Member, pageable: Pageable): Page<Question>
 }
