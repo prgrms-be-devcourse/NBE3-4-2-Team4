8f5aa750b06bfea76f701e28ec0d679a1d6c45ac
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-03-07 15:53:00 +0900

    refactor - Async, EnableJpaAuditing, Mail 관련 Config 클래스 코틀린화

---
+++ backend/src/main/java/com/NBE3_4_2_Team4/global/config/AsyncConfig.kt
@@ -0,0 +1,9 @@
+package com.NBE3_4_2_Team4.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+
+@EnableAsync
+@Configuration
+class AsyncConfig {
+}
