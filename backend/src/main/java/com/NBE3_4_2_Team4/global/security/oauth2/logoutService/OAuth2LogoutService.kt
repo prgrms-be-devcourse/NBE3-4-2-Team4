2ae94f17ed5c0c3a0255cc7d6a40e43395ec090e
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-03-10 11:50:29 +0900

    refactor - OAuth2LogoutService 코틀린화

---
+++ backend/src/main/java/com/NBE3_4_2_Team4/global/security/oauth2/logoutService/OAuth2LogoutService.kt
@@ -0,0 +1,23 @@
+package com.NBE3_4_2_Team4.global.security.oauth2.logoutService
+
+import com.NBE3_4_2_Team4.domain.member.member.entity.Member
+import org.springframework.beans.factory.annotation.Value
+
+abstract class OAuth2LogoutService {
+    @Value("\${custom.domain.backend}")
+    lateinit var backendDomain: String
+
+    companion object {
+        const val LOGOUT_COMPLETE_URL: String = "/api/logout/complete"
+    }
+
+    abstract fun getOAuth2Provider(): Member.OAuth2Provider?
+
+    fun getLogoutRedirectUrl(): String {
+        return backendDomain + LOGOUT_COMPLETE_URL
+    }
+
+    open fun getLogoutUrl(): String? {
+        return getLogoutRedirectUrl()
+    };
+}
